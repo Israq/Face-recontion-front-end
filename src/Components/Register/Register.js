513abd54704586b26c01051693e5b32f03c4b0f4
Syed Ragib Israq <ragib.israq9833@gmail.com>
2023-02-21 14:26:28 +0600

    spell

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -19,7 +19,7 @@ class Register extends  React.Component {
         this.setState({ password: event.target.value })
     }
     onSubmitSignIn = () => {
-        fetch('https://face-recognition-backend-r8nm.onrender.com/register',{
+        fetch('https://face-recognition-backend-r8nm.onrender.com/Register',{
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
