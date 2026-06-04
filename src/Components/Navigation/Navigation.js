6a5d1c043489edaa406effb0e533a85d7e725570
Israq <ragib.israq9833@gmail.com>
2026-06-04 18:19:40 +0600

    CSS

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -1,19 +1,34 @@
-import React from  'react';
+import React from "react";
 
 const Navigation = ({ onRoutechange, isSignedIn }) => {
-    if(isSignedIn){
+  if (isSignedIn) {
     return (
-        <nav style={{ display: 'flex', justifyContent: 'flex-end'}}>
-            <p onClick={()=>onRoutechange('SignOut')} className='f3 link dim black underline pa3 pointer'>Sign Out</p>
-        </nav>
-     );
-    } else {
-        return(
-            <nav style={{ display: 'flex', justifyContent: 'flex-end'}}>
-                <p onClick={()=>onRoutechange('SignIn')} className='f3 link dim black underline pa3 pointer'>Sign In</p>
-                <p onClick={()=>onRoutechange('register')} className='f3 link dim black underline pa3 pointer'>Register</p>
-            </nav>
-        );
-    }
-}
+      <nav style={{ display: "flex", justifyContent: "flex-end" }}>
+        <p
+          onClick={() => onRoutechange("SignOut")}
+          className="f3 link dim black  pa3 pointer"
+        >
+          Sign Out
+        </p>
+      </nav>
+    );
+  } else {
+    return (
+      <nav style={{ display: "flex", justifyContent: "flex-end" }}>
+        <p
+          onClick={() => onRoutechange("SignIn")}
+          className="f3 link dim black underline pa3 pointer"
+        >
+          Sign In
+        </p>
+        <p
+          onClick={() => onRoutechange("register")}
+          className="f3 link dim black underline pa3 pointer"
+        >
+          Register
+        </p>
+      </nav>
+    );
+  }
+};
 export default Navigation;
