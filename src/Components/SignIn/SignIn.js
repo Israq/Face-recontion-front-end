ddf88b221b5474f25d3ccde9c6b8e024c3a95b60
Israq <ragib.israq9833@gmail.com>
2026-06-11 01:29:27 +0600

    fix api urls for production

--- src/Components/SignIn/SignIn.js
+++ src/Components/SignIn/SignIn.js
@@ -17,7 +17,7 @@ class SignIn extends React.Component {
     }
 
     onSubmitSignIn = () => {
-        fetch('http://localhost:3001/signin',{
+        fetch('https://face-recognition-backend-r8nm.onrender.com/signin',{
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
